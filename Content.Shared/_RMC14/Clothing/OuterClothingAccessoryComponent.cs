2e3142c8f82d736b1ee6cf7ed69a054e5cfdf2dd
Troy Ramezani <troyramezani4@gmail.com>
2025-06-03 03:01:49 -0700

    alot

---
+++ Content.Shared/_RMC14/Clothing/OuterClothingAccessoryComponent.cs
@@ -0,0 +1,15 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Utility;
+
+namespace Content.Shared._RMC14.Clothing;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(OuterClothingAccessoriesSystem))]
+public sealed partial class OuterClothingAccessoryComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public SpriteSpecifier.Rsi Rsi;
+
+    [DataField, AutoNetworkedField]
+    public SpriteSpecifier.Rsi? ToggledRsi;
+}
---
+++ Content.Shared/_RMC14/Clothing/OuterClothingAccessoryHolderComponent.cs
@@ -0,0 +1,11 @@
+﻿using Robust.Shared.GameStates;
+using Content.Shared.Inventory;
+
+namespace Content.Shared._RMC14.Clothing;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class OuterClothingAccessoryHolderComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public SlotFlags Slot = SlotFlags.OUTERCLOTHING;
+}
